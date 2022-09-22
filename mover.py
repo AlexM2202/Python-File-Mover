74e05498b1002f5d0acf58a4ab590effedffd107
AlexM2202 <alexmarqu22@gmail.com>
2022-09-22 18:57:24 +0000

    excluded mover.py

--- mover.py
+++ mover.py
@@ -8,6 +8,8 @@ src = os.fspath(os.getcwd())
 dest = input("Where is this going to - ")
 
 for file in os.listdir():
+    if file == "mover.py":
+        continue
     if os.path.isdir(file):
         finial = shutil.move(src, dest)
         #print(shutil.move(src,dest))
