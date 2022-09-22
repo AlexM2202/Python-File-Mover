b54ca24395574e55eec02da279a2416aabec42ab
AlexM2202 <alexmarqu22@gmail.com>
2022-09-22 18:33:29 +0000

    file mover

---
+++ mover.py
@@ -0,0 +1,14 @@
+#Alex Marquardt
+
+import os
+import shutil
+
+src = os.fspath(os.getcwd())
+
+dest = input("Where is this going to - ")
+
+for file in os.listdir():
+    if os.path.isdir(file):
+        finial = shutil.move(src, dest)
+        #print(shutil.move(src,dest))
+        print("Destination path:", finial)
