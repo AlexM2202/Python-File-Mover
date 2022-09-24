2372611c0909bf4dec7e61a26a672a5bbf02cda1
AlexM2202 <alexmarqu22@gmail.com>
2022-09-24 00:31:35 +0000

    trying ssh

--- mover.py
+++ mover.py
@@ -1,15 +1,30 @@
-#Alex Marquardt
+#Alex Marquardt with help from Grand Duchars
 
+#inports
+import paramiko
 import os
 import shutil
 
-src = os.fspath(os.getcwd())
+#gathering ssh information
+address = input("Please input the server url - ")
+user = input("Please input the username - ")
+secret = input("Please input the password - ")
 
+#accessing ssh
+ssh = paramiko.SSHClient()
+ssh.connect(address, username=user, password=secret)
+
+#gathering path and destionation
+src = os.fspath(os.getcwd())
 dest = input("Where is this going to - ")
 
+#Loop for detecting and moving files
 for file in os.listdir():
-    if file == "mover.py" or file == ".git":
-        continue
+    # if file == "":
+    #     continue
     if os.path.isdir(file):
         finial = shutil.move(src + "/" + file, dest)
         print("Destination path:", finial)
+
+#closing ssh
+ssh.close()
