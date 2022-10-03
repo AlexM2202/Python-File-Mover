ab2121ba92ee153652a4fe0f08497b99b465c664
AlexM2202 <alexmarqu22@gmail.com>
2022-10-03 20:25:02 +0000

    updates

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
-#!/bash/bin
-sudo apt install pip -y
+#!bash/bin
+
+sudo apt install pip
 echo "pip installed!"
 
 sudo pip install paramiko
 echo "paramiko installed!"
 
-echo "Alex's file mover install complete!"
+echo "Alex's file mover setup complete!"
+
 sudo rm install.sh
+sudo rm pbxInstall.sh
---
+++ pbxInstall.sh
@@ -0,0 +1,21 @@
+#!/bash/bin
+
+yum -y update
+yum -y install yum-utils
+yum -y groupinstall development
+yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+yum -y install python36u
+
+ln -s /usr.local/bin/python3.6 /usr/local/bin/python3
+
+yum install python36u-pip
+echo "pip installed!"
+
+pip3.6 install paramiko
+echo "paramiko installed!"
+
+yum -y install python36u-devel
+
+echo "Alex's file mover install complete!"
+rm pbxInstall.sh
+rm install.sh
