4c5c2722a4ea21d04dcc318c18e367e18160a610
AlexM2202 <alexmarqu22@gmail.com>
2022-10-03 16:31:25 +0000

    updates

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bash/bin
-sudo apt install pip
-sudo pip install paramiko
+sudo apt install pip -y
+sudo pip install paramiko -y
 sudo rm -R file
 sudo rm -R file2
-sudo rm -R plzmove
+sudo rm -R plzMove
 echo "install complete"
+sudo rm install.sh
