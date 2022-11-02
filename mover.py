dbb6415f06c0208010e6c1ae87c40294eeb68a49
AlexM2202 <alexmarqu22@gmail.com>
2022-11-02 19:03:42 +0000

    recursion update

--- mover.py
+++ mover.py
@@ -10,72 +10,93 @@ from pathlib import Path
 # from tqdm import tqdm
 
 # Console text formatting
-YELLOWTXT = '\033[33m'
-GREENTXT = '\033[32m'
-BLUETXT = '\033[036m'
-RESETTXT = '\033[39m'
+YELLOWTXT = "\033[33m"
+GREENTXT = "\033[32m"
+BLUETXT = "\033[036m"
+RESETTXT = "\033[39m"
 
-# Gathering ssh information
-address = input(YELLOWTXT + "Please input the server url:" + RESETTXT)
-user = input(YELLOWTXT + "Please input the username:" + RESETTXT)
-secret = getpass.getpass(YELLOWTXT + "Please input the password:" + RESETTXT)
-
-# Gathering path and destionation
-src = input(YELLOWTXT + "Where is the Source:" + RESETTXT)
-dest = input(YELLOWTXT + "Where is this going to:" + RESETTXT)
-
-#setting up log file
-log = Path()
-log = log.resolve()
-log = str(log) + "/log.txt"
-fp = open(log, 'a')
-logging.basicConfig(filename=log, level=logging.DEBUG, format= "%(asctime)s %(message)s", filemode='a')
-
-# Accessing ssh
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(address, username=user, password=secret)
-ftp_client = ssh_client.open_sftp()
-
-ftp_client.chdir(dest)
-
-print (GREENTXT + "start!" + RESETTXT)
+# Files moved counter
 
 counter = 0
-p = Path(src)
-for year in p.iterdir():
-    if (year.is_dir() == False):
-        continue
-    try:
-        ftp_client.mkdir(year.name)
-    except IOError:
-        logging.info(str(year.name) + " already existed!")
-    for month in year.iterdir():
-        try:
-            ftp_client.mkdir(year.name + "/" + month.name)
-        except IOError:
-            logging.info(str(month.name) + " already existed!")
-        for day in month.iterdir():
+
+def main():
+    inputs = open('inputs.txt', 'r')
+
+    # auto inputs from inputs.txt
+    line = inputs.readline()
+    address = line.strip()
+    line = inputs.readline()
+    user = line.strip()
+    line = inputs.readline()
+    secret = line.strip()
+    line = inputs.readline()
+    src = line.strip()
+    line = inputs.readline()
+    dest = line.strip()
+
+    inputs.close()
+
+    # Gathering ssh information
+    # address = input(f"{YELLOWTXT}Please input the server url:{RESETTXT}")
+    # user = input(f"{YELLOWTXT}Please input the username:{RESETTXT}")
+    # secret = getpass.getpass(f"{YELLOWTXT}Please input the password:{RESETTXT}")
+
+    # Gathering path and destionation
+    # src = input(f"{YELLOWTXT}Where is the Source:{RESETTXT}")
+    # dest = input(f"{YELLOWTXT}Where is this going to:{RESETTXT}")
+
+    # Setting up log file
+    log = Path()
+    log = log.resolve()
+    log = f"{log}/log.txt"
+    fp = open(log, 'a')
+    logging.basicConfig(filename=log, level=logging.DEBUG, format= "%(asctime)s %(message)s", filemode='a')
+
+    # Accessing ssh
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(address, username=user, password=secret)
+    global ftp_client 
+    ftp_client = ssh_client.open_sftp()
+
+    ftp_client.chdir(dest)
+
+    # Notify moving start
+    print(f"{GREENTXT}Start!{RESETTXT}")
+
+    # Run through and move filetree to nas
+    parent = Path(src)
+    findAndMove(parent, dest)
+
+    # Finishing up
+    print (f"{counter} file(s) were moved.")
+    print(f"{GREENTXT}Done!{RESETTXT}")
+    
+    # Closing ftp and ssh
+    ftp_client.close()
+    ssh_client.close()
+    fp.write("\n Done! \n\n")
+    fp.close()
+
+def findAndMove(parent, remotePath):
+    for child in parent.iterdir():
+        if (child.is_dir() == False):
+            print(f"{BLUETXT}{child}{RESETTXT}")
+            # Move file to nas
+            ftp_client.put(f"{child}",f"{remotePath}/{child.name}")
+            logging.info(f"{child} --> {remotePath}/{child.name}")
+            # Increment files moved counter
+            global counter
+            counter = counter + 1
+            # Remove file from original
+            os.remove(child)
+        else:
             try:
-                ftp_client.mkdir(year.name +"/" + month.name + "/" + day.name)
+                ftp_client.mkdir(f"{remotePath}/{child.name}")
             except IOError:
-                logging.info(str(day.name) + " already existed!")
-            for file in day.iterdir():
-                print(BLUETXT + str(file) + RESETTXT)
-                ftp_client.put(str(day) + "/" + file.name, dest + "/" + year.name + "/" + month.name + "/" + day.name + "/" + file.name)
-                logging.info(str(day) + "/" + file.name + " --> " + dest + "/" + year.name + "/" + month.name + "/" + day.name + "/" + file.name)
-                counter = counter + 1
-            os.remove(file)
-        day.rmdir()
-    month.rmdir()
-year.rmdir()
+                logging.info(f"{child.name} alread exists")
+            findAndMove(child, f"{remotePath}/{child.name}")
+            child.rmdir()
 
-print (str(counter) + " files were moved.")
-print(GREENTXT + "Done!" + RESETTXT)
-  
-# Closing ftp and ssh
-
-ftp_client.close()
-ssh_client.close()
-fp.write("\n Done! \n\n")
-fp.close()
+if __name__ == "__main__":
+    main()
